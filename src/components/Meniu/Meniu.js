1deb542721d767c257cc290661b8220254e16b75
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-12-16 11:43:26 +0200

    create app and basic routes

---
+++ src/components/Meniu/Meniu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Meniu = () => {
+  return (
+    <div>
+      <ul>
+        <li>supa</li>
+        <li>gratar</li>
+        <li>friptura</li>
+        <li>cartofi</li>
+      </ul>
+    </div>
+  );
+};

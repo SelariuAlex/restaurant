e8ffdf5f243d49e0bf8e0fca88d50a05ea454d9a
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-12-21 12:57:52 +0200

    hamburger meniu

---
+++ src/components/Navbar/Hamburger/Hamburger.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./hamburger.styles.css";
+
+export const Hamburger = () => {
+  return (
+    <div className="hamburger">
+      <div className="hamburger__line"></div>
+      <div className="hamburger__line"></div>
+      <div className="hamburger__line"></div>
+    </div>
+  );
+};
